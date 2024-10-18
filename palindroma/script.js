c51983b71d41d0c98826c162cb0d0cc4a2ddfdce
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-18 22:52:14 +0200

    added function reverseString

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,5 +1,27 @@
 // 1) Chiedere all’utente di inserire una parola
 
-let word = prompt("Inserisci una parola");
-console.log(word);
+let inputWord = prompt("Inserisci una parola");
+
+console.log(reverseString(inputWord));
+
+
+// 2) Creare una funzione per capire se la parola inserita è palindroma.
+
+function reverseString(str) {
+    // Step 1. Usa il metodo split() per restituire un nuovo array
+    let splitString = str.split(""); // var splitString = "hello".split("");
+ 
+    // Step 2. Usa il metodo reverse() per invertire l'array appena creato
+    let reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+ 
+    // Step 3. Usa il metodo join() per unire tutti gli elementi della stringa in un array
+    let joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+    
+    //Step 4. Restituisci valore booleano
+    return joinArray === str;
+
+}
+
 
