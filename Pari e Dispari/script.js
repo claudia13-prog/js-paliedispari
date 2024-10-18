cac4c27d9f4746152080839bbf2f78ff37170a70
claudia13-prog <cappellaclaudia13@gmail.com>
2024-10-18 23:22:21 +0200

    added input

---
+++ Pari e Dispari/script.js
@@ -0,0 +1,22 @@
+// 1) L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+const userChoice = prompt("Scegli 'pari' o 'dispari'");
+const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5:"));
+
+console.log(userChoice);
+console.log(userNumber);
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+
+
+
+
+
+
+
+
+
+
+
+// Function
